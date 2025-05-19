d667fbc7fd9d2475344ca44a877cc0f0e91e7085
Achigyus <gyuseachi@yahoo.com>
2025-05-19 05:01:36 +0100

    add toggle menu functionality

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,1 +1,19 @@
 import '../sass/index.sass'
+import img from '../assets/images/icon-previous.svg'
+
+let menuBtn = document.getElementById('menu_btn') as HTMLButtonElement
+let menuUl = document.getElementById('nav_links_ul') as HTMLUListElement
+let openMenuIcon = menuBtn.querySelector('.open') as HTMLImageElement
+let closeMenuIcon = menuBtn.querySelector('.close') as HTMLImageElement
+let navBtns = document.querySelectorAll('.scroll_btn') as NodeListOf<HTMLButtonElement>
+let navRight = document.querySelector('.scroll_btn_img.right') as HTMLImageElement
+
+console.log(navRight)
+
+function toggleMenu() {
+  menuUl.classList.toggle('active')
+  openMenuIcon.classList.toggle('hidden')
+  closeMenuIcon.classList.toggle('hidden')
+}
+
+menuBtn.addEventListener('click', toggleMenu)
